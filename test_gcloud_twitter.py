25f70ec0b4fde4ac0131bd43117ac2222ec4729f
Jie1995tbc <55373808+Jie1995tbc@users.noreply.github.com>
2020-02-09 19:51:08 -0500

    Update test_gcloud_twitter.py

--- test_gcloud_twitter.py
+++ test_gcloud_twitter.py
@@ -3,4 +3,4 @@ from gcloud_tweepy import tweepy_api
 
 if __name__ == '__main__':
     queue = input("What would you like to search?")
-    assert tweepy_api(queue) is not '0'
+    assert tweepy_api(queue) != '0'
